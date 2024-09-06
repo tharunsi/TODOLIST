21ab31d855627e3d9cc4dce1b23d439d72a019d4
tharunsi Name <tharunsitharunsi@gmail.com>
2024-09-06 13:12:37 +0530

    vite-commit

--- todolist/vite.config.js
+++ todolist/vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/todolist/"
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  }
 })
